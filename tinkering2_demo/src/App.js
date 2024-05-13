b4a4046862f427b4b5d72187800e50d70a59de76
hugemartyr <128484610+hugemartyr@users.noreply.github.com>
2024-05-14 01:45:57 +0530

    #

---
+++ tinkering2_demo/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from './Footer.js';
+import Header from './Header.js';
+import Main from './Main.js';
+import TopNav from './TopNav.js';
+import Buy from './Buy.js';
+
+
+function App(){
+  return (
+    <div className='App'>
+    
+    <Header />
+      <TopNav />
+      <Main />
+      <Buy/>
+      <Footer />
+  
+    </div>
+  )
+
+}
+
+
+
+
+export default App;
